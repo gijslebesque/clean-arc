45f5acacc4350ef9890df99905497036d9e8dfd0
Gijs Lebesque <gijslebesque@gijss-mbp-2.home>
2022-10-25 10:58:13 +0300

    chore: init

---
+++ src/lib/entity.ts
@@ -0,0 +1,33 @@
+import UniqueEntityID from 'cuid';
+
+// eslint-disable-next-line no-use-before-define
+const isEntity = <T>(v: Entity<T>): v is Entity<T> => v instanceof Entity;
+
+class Entity<T> {
+  protected readonly _id: string;
+
+  protected props: T;
+
+  constructor(props: T, id?: string) {
+    this._id = id || UniqueEntityID();
+    this.props = props;
+  }
+
+  public equals(object?: Entity<T>): boolean {
+    if (object === null || object === undefined) {
+      return false;
+    }
+
+    if (this === object) {
+      return true;
+    }
+
+    if (!isEntity(object)) {
+      return false;
+    }
+
+    return this._id === object._id;
+  }
+}
+
+export default Entity;
