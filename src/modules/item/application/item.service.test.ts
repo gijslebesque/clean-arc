b5025d7700b865b762fd036b84be2dc22a56574a
Gijs Lebesque <gijslebesque@gijss-mbp-2.home>
2022-11-02 14:38:21 +0200

    feat: test for memory controller

---
+++ src/modules/item/application/item.service.test.ts
@@ -0,0 +1,60 @@
+import "reflect-metadata";
+import { Container } from "inversify";
+import { ItemRepository } from "../domain/repository/item.respository";
+import { ItemMemoryRepository } from "../infra/database/memory/item.memory.repository";
+import { TYPES } from "../types";
+import { ItemService } from "./item.service";
+import { MemoryData } from "../../../infra/database/memory/memory-db";
+import { Item } from "../domain/entity/item.entity";
+
+const testEntity = Item.create({ displayName: "hello", price: 1 });
+
+describe("item-service", () => {
+  // use container for necessary dependency injections
+  const container = new Container();
+  container.bind(TYPES.Database).to(MemoryData).inSingletonScope();
+  container.bind<ItemRepository>(TYPES.ItemRepository).to(ItemMemoryRepository);
+  container.bind(TYPES.ItemService).to(ItemService);
+  const itemService: ItemService = container.get(TYPES.ItemService);
+
+  test("is defined", () => {
+    expect(itemService).toBeDefined();
+  });
+
+  test("creates an item", async () => {
+    // Arrange
+
+    // Act
+    const newItem = await itemService.create(testEntity);
+
+    // Assert
+    expect(testEntity).toEqual(newItem);
+  });
+
+  test("finds a items", async () => {
+    // Act
+
+    const res = await itemService.findAll();
+
+    // Assert
+    expect(res).toEqual([testEntity]);
+  });
+
+  test("finds an item by id", async () => {
+    // Act
+
+    const res = await itemService.getById(testEntity.id);
+
+    // Assert
+    expect(res).toEqual(testEntity);
+  });
+
+  test("throws error if item doesn't exist", async () => {
+    // Act
+    try {
+      await itemService.getById("xxx");
+    } catch (err) {
+      expect(err).toEqual(new Error("xxx"));
+    }
+  });
+});
